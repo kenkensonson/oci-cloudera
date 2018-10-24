4c926a75757c56755d19addbfac673645a78d520
Ben Lackey <ben.lackey@outlook.com>
2018-10-24 11:07:18 -0700

    delete copy and paste readme, refactor n-node

---
+++ n-node/utility.tf
@@ -0,0 +1,49 @@
+resource "oci_core_instance" "UtilityNode" {
+  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.AD - 1],"name")}"
+  compartment_id      = "${var.compartment_ocid}"
+  display_name        = "CDH Utility1"
+  hostname_label      = "CDH-Utility1"
+  shape               = "${var.MasterInstanceShape}"
+  subnet_id           = "${oci_core_subnet.public.*.id[var.AD - 1]}"
+
+  source_details {
+    source_type             = "image"
+    source_id               = "${var.InstanceImageOCID[var.region]}"
+    boot_volume_size_in_gbs = "${var.boot_volume_size}"
+  }
+
+  metadata {
+    ssh_authorized_keys = "${var.ssh_public_key}"
+    user_data           = "${base64encode(file("../scripts/boot.sh"))}"
+  }
+
+  timeouts {
+    create = "30m"
+  }
+}
+
+data "oci_core_vnic_attachments" "utility_node_vnics" {
+  compartment_id      = "${var.compartment_ocid}"
+  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.AD - 1],"name")}"
+  instance_id         = "${oci_core_instance.UtilityNode.id}"
+}
+
+data "oci_core_vnic" "utility_node_vnic" {
+  vnic_id = "${lookup(data.oci_core_vnic_attachments.utility_node_vnics.vnic_attachments[0],"vnic_id")}"
+}
+
+resource "oci_core_volume" "UtilityVolume" {
+  count               = "1"
+  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.AD - 1],"name")}"
+  compartment_id      = "${var.compartment_ocid}"
+  display_name        = "CDH Utility Volume"
+  size_in_gbs         = "${var.blocksize_in_gbs}"
+}
+
+resource "oci_core_volume_attachment" "UtilityAttachment" {
+  count           = "1"
+  attachment_type = "iscsi"
+  compartment_id  = "${var.compartment_ocid}"
+  instance_id     = "${oci_core_instance.UtilityNode.id}"
+  volume_id       = "${oci_core_volume.UtilityVolume.id}"
+}
