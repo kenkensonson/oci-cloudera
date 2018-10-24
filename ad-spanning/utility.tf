1cd5218b6f3164cb9bd8049f1467f262d1f4c48a
Ben Lackey <ben.lackey@outlook.com>
2018-10-24 11:49:02 -0700

    drop mod

--- ad-spanning/utility.tf
+++ ad-spanning/utility.tf
@@ -1,16 +1,15 @@
-resource "oci_core_instance" "UtilityNode" {
-  count               = "${var.UtilityNodeCount}"
-  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[count.index%3],"name")}"
+resource "oci_core_instance" "utility" {
+  count               = "${var.utility["node_count"]}"
+  availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[var.availability_domain], "name")}"
   compartment_id      = "${var.compartment_ocid}"
-  display_name        = "CDH Utility ${count.index+1}"
-  hostname_label      = "CDH-Utility-${count.index+1}"
-  shape               = "${var.MasterInstanceShape}"
-  subnet_id           = "${oci_core_subnet.public.*.id[count.index%3]}"
+  display_name        = "cdh-utility-${format("%01d", count.index+1)}"
+  hostname_label      = "cdh-utility-${format("%01d", count.index+1)}"
+  shape               = "${var.utility["shape"]}"
+  subnet_id           = "${oci_core_subnet.public.*.id[var.availability_domain]}"
 
   source_details {
-    source_type             = "image"
-    source_id               = "${var.InstanceImageOCID[var.region]}"
-    boot_volume_size_in_gbs = "${var.boot_volume_size}"
+    source_type = "image"
+    source_id   = "${var.images[var.region]}"
   }
 
   metadata {
@@ -23,12 +22,12 @@ resource "oci_core_instance" "UtilityNode" {
   }
 }
 
-data "oci_core_vnic_attachments" "utility_node_vnics" {
+data "oci_core_vnic_attachments" "utility_vnics" {
   compartment_id      = "${var.compartment_ocid}"
-  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[0],"name")}"
-  instance_id         = "${oci_core_instance.UtilityNode.*.id[0]}"
+  availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[var.availability_domain], "name")}"
+  instance_id         = "${oci_core_instance.utility.id}"
 }
 
-data "oci_core_vnic" "utility_node_vnic" {
-  vnic_id = "${lookup(data.oci_core_vnic_attachments.utility_node_vnics.vnic_attachments[0],"vnic_id")}"
+data "oci_core_vnic" "utility_vnic" {
+  vnic_id = "${lookup(data.oci_core_vnic_attachments.utility_vnics.vnic_attachments[0], "vnic_id")}"
 }
