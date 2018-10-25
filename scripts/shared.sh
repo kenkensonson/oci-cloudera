1b020aa4df0e41d78b6b1939b083678d5c522151
Ben Lackey <ben.lackey@outlook.com>
2018-10-25 11:32:09 -0700

    clean up shared

--- scripts/shared.sh
+++ scripts/shared.sh
@@ -1,9 +1,8 @@
 # Turn off selinux
-#sed -i.bak 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
-#setenforce 0
+sed -i.bak 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+setenforce 0
 
 # Set DNS to resolve all subnets
-rm -f /etc/resolv.conf
 echo "search public0.cloudera.oraclevcn.com public1.cloudera.oraclevcn.com public2.cloudera.oraclevcn.com private0.cloudera.oraclevcn.com private1.cloudera.oraclevcn.com private2.cloudera.oraclevcn.com" > /etc/resolv.conf
 echo "nameserver 169.254.169.254" >> /etc/resolv.conf
 
