fd726f131d6fba05839a8b3a444a515b7a835ace
Ben Lackey <ben.lackey@outlook.com>
2018-10-24 22:50:45 -0700

    simplify shared

--- scripts/shared.sh
+++ scripts/shared.sh
@@ -1,12 +1,6 @@
 # Turn off selinux
-if [ -f /etc/selinux/config ]; then
-  selinuxchk=`sudo cat /etc/selinux/config | grep enforcing`
-  selinux_chk=`echo -e $?`
-  if [ $selinux_chk = "0" ]; then
-    sudo sed -i.bak 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
-    sudo setenforce 0
-  fi
-fi
+sudo sed -i.bak 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+sudo setenforce 0
 
 # Set DNS to resolve all subnets
 sudo rm -f /etc/resolv.conf
