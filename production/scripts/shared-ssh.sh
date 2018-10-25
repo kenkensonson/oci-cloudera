c3d4f6b99ab447351d5a9fd003f6ab23e5cbf05f
Ben Lackey <ben.lackey@outlook.com>
2018-10-25 16:17:54 -0700

    fiddling w discovery

---
+++ production/scripts/shared-ssh.sh
@@ -0,0 +1,38 @@
+# Need to set up the hosts as so:
+# https://www.cloudera.com/documentation/enterprise/latest/topics/configure_network_names.html#configure_network_names
+
+discovery () {
+  # Utility
+  hname=`host cdh-utility`
+  echo "$hname" | head -n 1 | gawk '{print $1}'
+
+  # Master
+  endcheck=1
+  i=0
+  while [ "$endcheck" != 0 ]; do
+    hname=`host cdh-master${i}`
+    hchk=$?
+    if [ "$hchk" = "1" ]; then
+      endcheck="0"
+    else
+      echo "$hname" | head -n 1 | gawk '{print $1}'
+      endcheck="1"
+    fi
+    i=$((i+1))
+  done;
+
+  # Worker
+  endcheck=1
+  i=0
+  while [ "$endcheck" != 0 ]; do
+    hname=`host cdh-worker${i}`
+    hchk=$?
+    if [ "$hchk" = "1" ]; then
+      endcheck="0"
+    else
+      echo "$hname" | head -n 1 | gawk '{print $1}'
+      endcheck="1"
+    fi
+    i=$((i+1))
+  done;
+}
--- production/scripts/shared.sh
+++ production/scripts/shared.sh
@@ -1,14 +1,11 @@
-# Turn off selinux
+echo "Turning off selinux..."
 sed -i.bak 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 setenforce 0
-
-# Set DNS to resolve all subnets
-echo "search public0.cloudera.oraclevcn.com public1.cloudera.oraclevcn.com public2.cloudera.oraclevcn.com private0.cloudera.oraclevcn.com private1.cloudera.oraclevcn.com private2.cloudera.oraclevcn.com" > /etc/resolv.conf
-echo "nameserver 169.254.169.254" >> /etc/resolv.conf
-
-# Need to set up the hosts as so:
-#https://www.cloudera.com/documentation/enterprise/latest/topics/configure_network_names.html#configure_network_names
 
 echo "Turning off the Firewall..."
 service firewalld stop
 chkconfig firewalld off
+
+# Set DNS to resolve all subnets
+echo "search public0.cloudera.oraclevcn.com public1.cloudera.oraclevcn.com public2.cloudera.oraclevcn.com private0.cloudera.oraclevcn.com private1.cloudera.oraclevcn.com private2.cloudera.oraclevcn.com" > /etc/resolv.conf
+echo "nameserver 169.254.169.254" >> /etc/resolv.conf
