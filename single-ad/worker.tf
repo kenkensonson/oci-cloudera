e734468013bf6793642dd029b62b83850e5da86f
Ben Lackey <ben.lackey@outlook.com>
2018-10-24 21:51:39 -0700

    bad ad code

--- single-ad/worker.tf
+++ single-ad/worker.tf
@@ -24,7 +24,7 @@ resource "oci_core_instance" "worker" {
 
 resource "oci_core_volume" "worker0" {
   count = "${var.worker["node_count"]}"
-  availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[count.index%3],"name")}"
+  availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[var.availability_domain], "name")}"
   compartment_id = "${var.compartment_ocid}"
   display_name = "cdh-worker${count.index}-volume0"
   size_in_gbs = "${var.worker["size_in_gbs"]}"
@@ -37,85 +37,3 @@ resource "oci_core_volume_attachment" "worker0" {
   instance_id = "${oci_core_instance.worker.*.id[count.index]}"
   volume_id = "${oci_core_volume.worker0.*.id[count.index]}"
 }
-
-/*
-resource "oci_core_volume" "worker1" {
-  count = "${var.worker["node_count"]}"
-  availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[count.index%3],"name")}"
-  compartment_id = "${var.compartment_ocid}"
-  display_name = "cdh-worker${count.index}-volume1"
-  size_in_gbs = "${var.worker["size_in_gbs"]}"
-}
-
-resource "oci_core_volume_attachment" "worker1" {
-  count = "${var.worker["node_count"]}"
-  attachment_type = "iscsi"
-  compartment_id = "${var.compartment_ocid}"
-  instance_id = "${oci_core_instance.worker.*.id[count.index]}"
-  volume_id = "${oci_core_volume.worker1.*.id[count.index]}"
-}
-
-resource "oci_core_volume" "worker2" {
-  count = "${var.worker["node_count"]}"
-  availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[count.index%3],"name")}"
-  compartment_id = "${var.compartment_ocid}"
-  display_name = "cdh-worker${count.index}-volume2"
-  size_in_gbs = "${var.worker["size_in_gbs"]}"
-}
-
-resource "oci_core_volume_attachment" "worker2" {
-  count = "${var.worker["node_count"]}"
-  attachment_type = "iscsi"
-  compartment_id = "${var.compartment_ocid}"
-  instance_id = "${oci_core_instance.worker.*.id[count.index]}"
-  volume_id = "${oci_core_volume.worker2.*.id[count.index]}"
-}
-
-resource "oci_core_volume" "worker3" {
-  count = "${var.worker["node_count"]}"
-  availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[count.index%3],"name")}"
-  compartment_id = "${var.compartment_ocid}"
-  display_name = "cdh-worker${count.index}-volume3"
-  size_in_gbs = "${var.worker["size_in_gbs"]}"
-}
-
-resource "oci_core_volume_attachment" "worker3" {
-  count = "${var.worker["node_count"]}"
-  attachment_type = "iscsi"
-  compartment_id = "${var.compartment_ocid}"
-  instance_id = "${oci_core_instance.worker.*.id[count.index]}"
-  volume_id = "${oci_core_volume.worker3.*.id[count.index]}"
-}
-
-resource "oci_core_volume" "worker4" {
-  count = "${var.worker["node_count"]}"
-  availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[count.index%3],"name")}"
-  compartment_id = "${var.compartment_ocid}"
-  display_name = "cdh-worker${count.index}-volume4"
-  size_in_gbs = "${var.worker["size_in_gbs"]}"
-}
-
-resource "oci_core_volume_attachment" "worker4" {
-  count = "${var.worker["node_count"]}"
-  attachment_type = "iscsi"
-  compartment_id = "${var.compartment_ocid}"
-  instance_id = "${oci_core_instance.worker.*.id[count.index]}"
-  volume_id = "${oci_core_volume.worker4.*.id[count.index]}"
-}
-
-resource "oci_core_volume" "worker5" {
-  count = "${var.worker["node_count"]}"
-  availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[count.index%3],"name")}"
-  compartment_id = "${var.compartment_ocid}"
-  display_name = "cdh-worker${count.index}-volume5"
-  size_in_gbs = "${var.worker["size_in_gbs"]}"
-}
-
-resource "oci_core_volume_attachment" "worker5" {
-  count = "${var.worker["node_count"]}"
-  attachment_type = "iscsi"
-  compartment_id = "${var.compartment_ocid}"
-  instance_id = "${oci_core_instance.worker.*.id[count.index]}"
-  volume_id = "${oci_core_volume.worker5.*.id[count.index]}"
-}
-*/
