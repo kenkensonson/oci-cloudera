eaafff9a8f5246b571204b0e84189b65ab5d831b
Ben Lackey <ben.lackey@outlook.com>
2018-10-25 11:30:18 -0700

    drop bastion from resolve.conf

--- scripts/shared.sh
+++ scripts/shared.sh
@@ -4,7 +4,7 @@
 
 # Set DNS to resolve all subnets
 rm -f /etc/resolv.conf
-echo "search public0.cloudera.oraclevcn.com public1.cloudera.oraclevcn.com public2.cloudera.oraclevcn.com private0.cloudera.oraclevcn.com private1.cloudera.oraclevcn.com private2.cloudera.oraclevcn.com bastion0.cloudera.oraclevcn.com bastion1.cloudera.oraclevcn.com bastion2.cloudera.oraclevcn.com" > /etc/resolv.conf
+echo "search public0.cloudera.oraclevcn.com public1.cloudera.oraclevcn.com public2.cloudera.oraclevcn.com private0.cloudera.oraclevcn.com private1.cloudera.oraclevcn.com private2.cloudera.oraclevcn.com" > /etc/resolv.conf
 echo "nameserver 169.254.169.254" >> /etc/resolv.conf
 
 echo "Turning off the Firewall..."
