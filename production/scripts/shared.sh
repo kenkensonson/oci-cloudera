66ec98aedd9f5e0f66046eb981cd8469b8724a50
Ben Lackey <ben.lackey@outlook.com>
2018-10-25 16:45:54 -0700

    more comments

--- production/scripts/shared.sh
+++ production/scripts/shared.sh
@@ -6,6 +6,6 @@ echo "Turning off the Firewall..."
 service firewalld stop
 chkconfig firewalld off
 
-# Set DNS to resolve all subnets
+echo "Setting up resolve.conf..."
 echo "search public0.cloudera.oraclevcn.com public1.cloudera.oraclevcn.com public2.cloudera.oraclevcn.com private0.cloudera.oraclevcn.com private1.cloudera.oraclevcn.com private2.cloudera.oraclevcn.com" > /etc/resolv.conf
 echo "nameserver 169.254.169.254" >> /etc/resolv.conf
