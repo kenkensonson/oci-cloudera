228fee698bf399d71de73015145d2c506af2fac5
Ben Lackey <ben.lackey@outlook.com>
2018-10-26 13:08:07 -0700

    this probably works...

--- production/scripts/utility.sh
+++ production/scripts/utility.sh
@@ -18,16 +18,11 @@ pip install psycopg2==2.7.5 --ignore-installed
 
 # Step 4.1: Make sure that LC_ALL is set to en_US.UTF-8 and initialize the database
 echo 'LC_ALL="en_US.UTF-8"' >> /etc/locale.conf
-
-#su -l postgres -c "postgresql-setup initdb"
-# wonder if we ought to do this instead....
-sudo service postgresql initdb
+su -l postgres -c "postgresql-setup initdb"
 
 # Step 4.2: Enable MD5 authentication.
 CONF_FILE=/var/lib/pgsql/data/pg_hba.conf
 sed -i '/host.*127.*ident/i \host    all         all         127.0.0.1/32          md5  \ ' ${CONF_FILE}
-
-# Need to fix this
 #sed -e '/^listen_addresses\s*=/d' -i ${CONF_FILE}
 
 # Step 4.3: Configure settings to ensure your system performs as expected.
@@ -37,32 +32,19 @@ sed -i '/host.*127.*ident/i \host    all         all         127.0.0.1/32       
 #sed -e '/^standard_conforming_strings\s*=/d' -i "$CONF_FILE"
 
 # Step 4.4: Configure the PostgreSQL server to start at boot.
-#systemctl enable postgresql
-#systemctl restart postgresql
-
-service postgresql start
-chkconfig postgresql on
+systemctl enable postgresql
+systemctl restart postgresql
 
 # Step 5: Set up the Cloudera Manager Database
-create_database()
-{
-  local DBNAME=$1
-  local PASSWORD=$2
-  local ROLE=$DBNAME
-
-  sudo -u postgres psql -c "CREATE ROLE scm LOGIN PASSWORD '${PASSWORD}';"
-  sudo -u postgres psql -c "CREATE DATABASE ${DBNAME} OWNER ${ROLE} ENCODING 'UTF8';"
-  sudo -u postgres psql -c "ALTER DATABASE ${DBNAME} SET standard_conforming_strings=off;"
-}
-
-
+for DATABASENAME in scm amon rman hue metastore sentry nav navms oozie; do
+  sudo -u postgres psql -c "CREATE ROLE ${DATABASENAME} LOGIN PASSWORD 'password';"
+  sudo -u postgres psql -c "CREATE DATABASE ${DATABASENAME} OWNER ${DATABASENAME} ENCODING 'UTF8';"
+  sudo -u postgres psql -c "ALTER DATABASE ${DATABASENAME} SET standard_conforming_strings=off;"
+done
 /opt/cloudera/cm/schema/scm_prepare_database.sh postgresql scm scm password
-
-
-
 
 # Step 6: Install CDH and Other Software
 systemctl start cloudera-scm-server
 
 # Step 7: Set Up a Cluster
-python utility-cmx.py
+#python utility-cmx.py
