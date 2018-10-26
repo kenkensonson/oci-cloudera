aaab22cfed80cd36895dc755bedd1344e917a495
Ben Lackey <ben.lackey@outlook.com>
2018-10-26 09:45:06 -0700

    reorder to mirror doc

--- production/scripts/shared.sh
+++ production/scripts/shared.sh
@@ -1,11 +1,11 @@
-echo "Turning off selinux..."
-sed -i.bak 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
-setenforce 0
+echo "Setting up resolve.conf..."
+echo "search public0.cloudera.oraclevcn.com public1.cloudera.oraclevcn.com public2.cloudera.oraclevcn.com private0.cloudera.oraclevcn.com private1.cloudera.oraclevcn.com private2.cloudera.oraclevcn.com" > /etc/resolv.conf
+echo "nameserver 169.254.169.254" >> /etc/resolv.conf
 
 echo "Turning off the Firewall..."
 service firewalld stop
 chkconfig firewalld off
 
-echo "Setting up resolve.conf..."
-echo "search public0.cloudera.oraclevcn.com public1.cloudera.oraclevcn.com public2.cloudera.oraclevcn.com private0.cloudera.oraclevcn.com private1.cloudera.oraclevcn.com private2.cloudera.oraclevcn.com" > /etc/resolv.conf
-echo "nameserver 169.254.169.254" >> /etc/resolv.conf
+echo "Turning off selinux..."
+sed -i.bak 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+setenforce 0
