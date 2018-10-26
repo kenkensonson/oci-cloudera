c9e597352ef1f1526de61063412ca8c39edfd17e
Ben Lackey <ben.lackey@outlook.com>
2018-10-26 15:28:14 -0700

    lines

--- production/scripts/utility.sh
+++ production/scripts/utility.sh
@@ -48,7 +48,6 @@ done
 systemctl start cloudera-scm-server
 
 # Step 7: Set Up a Cluster
-#python utility-cmx.py
 
 ## Execute Python cluster setup
 VMSIZE=$(curl http://169.254.169.254/opc/v1/instance/shape)
@@ -63,6 +62,22 @@ JOBROLE="root"
 JOBFUNCTION="root"
 COMPANY="Oracle"
 
-#mkdir -p /log/cloudera
-#echo -e "Setup ready to execute... Running Cluster Initialization Script... (output will begin shortly)"
-#python /home/opc/cmx.py -a -n "$ClusterName" -u "$User" -m "$mip" -w "$cluster_host_ip" -c "$cmUser" -s "$cmPassword" -e -r "$EMAILADDRESS" -b "$BUSINESSPHONE" -f "$FIRSTNAME" -t "$LASTNAME" -o "$JOBROLE" -i "$JOBFUNCTION" -y "$COMPANY" -v "$VMSIZE" -k "$ssh_keypath"
+mkdir -p /log/cloudera
+python /home/opc/cmx.py \
+  -a \
+  -n "$ClusterName" \
+  -u "$User" \
+  -m "$mip" -w \
+  "$cluster_host_ip" \
+  -c "$cmUser" \
+  -s "$cmPassword" \
+  -e \
+  -r "$EMAILADDRESS" \
+  -b "$BUSINESSPHONE" \
+  -f "$FIRSTNAME" \
+  -t "$LASTNAME" \
+  -o "$JOBROLE" \
+  -i "$JOBFUNCTION" \
+  -y "$COMPANY" \
+  -v "$VMSIZE" \
+  -k "$ssh_keypath"
