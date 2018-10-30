b42d36222349a477b97dc863c533af0557afb47d
Ben Lackey <ben.lackey@outlook.com>
2018-10-30 15:22:16 -0700

    try with hammer

--- production/scripts/utility.sh
+++ production/scripts/utility.sh
@@ -22,7 +22,7 @@ ip=$(hostname -I)
 ip=${ip%?}
 echo "# TYPE  DATABASE        USER            ADDRESS                 METHOD
 local   all             all                                     peer
-host    all             all             ${ip}/32             md5
+host    all             all             0.0.0.0/0               md5
 host    all             all             127.0.0.1/32            ident
 host    all             all             ::1/128                 ident
 " > /var/lib/pgsql/data/pg_hba.conf
