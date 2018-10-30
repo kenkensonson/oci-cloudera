2c4f2993fce5e7b6b937779ab541c6054807755d
Ben Lackey <ben.lackey@outlook.com>
2018-10-30 13:11:27 -0700

    too many lines, not enough something

--- production/scripts/utility.sh
+++ production/scripts/utility.sh
@@ -29,13 +29,12 @@ ip=${ip%?}
 echo "# TYPE  DATABASE        USER            ADDRESS                 METHOD
 local   all             all                                     peer
 host    all             all             127.0.0.1/32            md5
-host    all             all             $(hostname -I)/32       md5
+host    all             all             ${ip}/32             md5
 host    all             all             127.0.0.1/32            ident
 host    all             all             ::1/128                 ident
 " > /var/lib/pgsql/data/pg_hba.conf
 
-echo "
-listen_addresses = '*'
+echo "listen_addresses = '*'
 " >> /var/lib/pgsql/data/postgresql.conf
 
 # Step 4.4: Configure the PostgreSQL server to start at boot.
