56d9904aabd67bcf3626532b3b2bab29e8e8bead
Ben Lackey <ben.lackey@outlook.com>
2018-10-30 17:55:04 -0700

    plan seems to work....

--- production/worker.tf
+++ production/worker.tf
@@ -1,11 +1,11 @@
 resource "oci_core_instance" "worker" {
   count               = "${var.worker["node_count"]}"
-  availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[count.index%var.availability_domains], "name")}"
+  availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[count.index % var.availability_domains], "name")}"
   compartment_id      = "${var.compartment_ocid}"
   display_name        = "worker${count.index}"
   hostname_label      = "worker${count.index}"
   shape               = "${var.worker["shape"]}"
-  subnet_id           = "${oci_core_subnet.private.*.id[count.index%var.availability_domains]}"
+  subnet_id           = "${oci_core_subnet.private.*.id[count.index % var.availability_domains]}"
 
   source_details {
     source_type = "image"
@@ -22,10 +22,10 @@ resource "oci_core_instance" "worker" {
 }
 
 resource "oci_core_volume" "worker" {
-  count               = "${var.worker["node_count"]}"
-  availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[count.index%var.availability_domains], "name")}"
+  count               = "${var.worker["node_count"] * var.worker["disk_count"]}"
+  availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[count.index % var.worker["node_count"] % var.availability_domains], "name")}"
   compartment_id      = "${var.compartment_ocid}"
-  display_name        = "worker${count.index%var.worker["node_count"]}-volume${floor(count.index/var.worker["node_count"])}"
+  display_name        = "worker${count.index%var.worker["node_count"]}-volume${count.index / var.worker["node_count"]}"
   size_in_gbs         = "${var.worker["size_in_gbs"]}"
 }
 
@@ -33,6 +33,6 @@ resource "oci_core_volume_attachment" "worker" {
   count           = "${var.worker["node_count"] * var.worker["disk_count"]}"
   attachment_type = "iscsi"
   compartment_id  = "${var.compartment_ocid}"
-  instance_id     = "${oci_core_instance.worker.*.id[count.index%var.worker["node_count"]]}"
-  volume_id       = "${oci_core_volume.worker.*.id[count.index]}"
+  instance_id     = "${oci_core_instance.worker.*.id[count.index % var.worker["node_count"]]}"
+  volume_id       = "${oci_core_volume.worker.*.id[count.index / var.worker["node_count"]]}"
 }
